07509540de7e8414c28302cda810fffc51af36b9
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-10-05 16:29:31 +0600

    add about section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {  Geist_Mono, Outfit } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const outfit = Outfit({subsets : ['latin'],variable: "--font-geist-mono",});
 
 export const metadata: Metadata = {
   title: "Rifat",
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${outfit.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,4 @@
-import { Calendar, GraduationCap, HomeIcon, MailIcon, PhoneCall, User2 } from "lucide-react";
+import { Briefcase, Calendar, GraduationCap, GraduationCapIcon, HomeIcon, MailIcon, PhoneCall, User2 } from "lucide-react";
 import Image from "next/image";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 
@@ -19,15 +19,31 @@ interface DataSection<T> {
     data: T[];
 }
 
+interface Education {
+    university: string;
+    qualification: string;
+    years: string;
+}
+
+interface Experience {
+    company: string;
+    qualification: string;
+    years: string;
+}
+
+type QualificationSection =
+    | { title: 'education'; data: Education[] }
+    | { title: 'experience'; data: Experience[] };
+
 const About = () => {
     const infoData = [
         {
             icon: <User2 size={20} />,
-            text: 'Ryan Davis',
+            text: 'Md Rifat Hossain',
         },
         {
             icon: <PhoneCall size={20} />,
-            text: 'Ryan Davis',
+            text: '01792842921',
         },
         {
             icon: <MailIcon size={20} />,
@@ -46,7 +62,7 @@ const About = () => {
             text: 'Dhaka , Bangladesh',
         },
     ]
-    const qualificationData = [
+    const qualificationData: QualificationSection[] = [
         {
             title: 'education',
             data: [
@@ -103,7 +119,7 @@ const About = () => {
             title: 'tools',
             data: [
                 {
-                    imgPath: '/assets/about/vscode.svg'
+                    imgPath: '/assets/vscode.svg'
                 },
                 {
                     imgPath: '/assets/about/figma.svg'
@@ -115,8 +131,13 @@ const About = () => {
         }
     ]
 
-    function getData<T>(arr: DataSection<T>[], title: string): DataSection<T> | undefined {
-        return arr.find((item) => item.title === title);
+
+
+    function getData<T extends QualificationSection['title']>(
+        data: QualificationSection[],
+        title: T
+    ): Extract<QualificationSection, { title: T }> | undefined {
+        return data.find((item): item is Extract<QualificationSection, { title: T }> => item.title === title)
     }
 
     return (
@@ -131,22 +152,209 @@ const About = () => {
             {/* image */}
             <div className="container mx-auto  flex flex-col xl:flex-row">
                 {/* image */}
-                <div className="hodden xl:flex flex-1 border bg-no-repeat relative">
+                <div className="hidden xl:flex flex-1  bg-no-repeat relative">
                     <Image src='/assets/rifat/banner-3.png' width={505} height={505} alt=''></Image>
                 </div>
 
-                <div className="flex-1 border">
-                    <Tabs  defaultValue='personal'>
-                        <TabsList className="w-full grid grid-cols-3 xl:max-w-[520px] xl:border dark:border-none bg-transparent p-0 border-none">
-                            <TabsTrigger value="personal">Personal</TabsTrigger>
-                            <TabsTrigger value="qualifications">Qualifications</TabsTrigger>
-                            <TabsTrigger value="skills">Skills</TabsTrigger>
+                <div className="flex-1">
+                    <Tabs className="" defaultValue='personal'>
+                        <TabsList className="w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none bg-transparent p-0 border-none">
+                            <TabsTrigger className="w-[162px] xl:w-auto" value="personal">Personal</TabsTrigger>
+                            <TabsTrigger className="w-[162px] xl:w-auto" value="qualifications">Qualifications</TabsTrigger>
+                            <TabsTrigger className="w-[162px] xl:w-auto" value="skills">Skills</TabsTrigger>
                         </TabsList>
-                        {/* personal */}
+
                         <div className="text-lg mt-12 xl:mt-8">
-                            <TabsContent value="personal">personal info</TabsContent>
-                            <TabsContent value="qualifications">qualifications info</TabsContent>
-                            <TabsContent value="skills">personal info</TabsContent>
+                            {/* personal */}
+                            <TabsContent className="mt-20 md:mt-0" value="personal">
+                                <div className="text-center xl:text-left">
+                                    <h3 className="h3 mb-4">Unmatched Service Quantity for Over 10 Years</h3>
+                                    <p className="subtitle max-w-xl mx-auto xl:mx-0">Special in crefting intutive with cutting-edge techology, delivering dynamic and engaging user experience</p>
+                                </div>
+                                {/* icon */}
+                                <div className="grid xl:grid-cols-2 gap-4 mb-12">
+                                    {
+                                        infoData.map((item, idx) => {
+                                            return (
+                                                <div key={idx} className="flex items-center gap-x-4 mx-auto xl:mx-0">
+                                                    <div className="text-primary">{item.icon}</div>
+                                                    <div>{item.text}</div>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+
+                                {/* languages */}
+                                <div className="flex flex-col gap-y-2">
+                                    <div className="text-primary">Language Skills</div>
+                                    <div className="border-b border-border"></div>
+                                    <div>English, Bangla</div>
+                                </div>
+                            </TabsContent>
+                            {/* qualifications */}
+                            <TabsContent className="mt-20 md:mt-0" value="qualifications">
+                                {/* <div>
+                                    <h3>My Awesome Journey</h3>
+                                </div> */}
+                                {/* experience and education wrapper */}
+                                <div className="grid md:grid-cols-2 gap-y-8">
+                                    {/* experience */}
+                                    <div>
+
+                                        <div className="flex gap-x-4 mb-4  item-center text-[22px]">
+                                            <Briefcase size={28} />
+                                            <h3 className="capitalize font-medium">
+                                                {getData(qualificationData, 'experience')?.title}
+                                            </h3>
+                                        </div>
+
+                                        {/* list */}
+                                        <div className="flex flex-col gap-y-10">
+                                            {getData(qualificationData, 'experience')?.data.map((item, idx) => {
+                                                const { company, qualification, years } = item;
+                                                return (
+                                                    <div key={idx} className="flex gap-x-6 group relative">
+                                                        {/* Timeline line + dot */}
+                                                        <div className="flex flex-col items-center relative">
+                                                            {/* Dot */}
+                                                            <div className="w-3 h-3 rounded-full bg-primary z-10 group-hover:translate-y-[84px] transition-all duration-500 "></div>
+                                                            {/* Line (extends downward) */}
+                                                            {idx !== getData(qualificationData, 'experience')!.data.length - 1 && (
+                                                                <div className="w-[2px] h-full bg-border mt-2"></div>
+                                                            )}
+                                                        </div>
+
+                                                        {/* Content */}
+                                                        <div>
+                                                            <h4 className="font-semibold text-xl leading-tight">{company}</h4>
+                                                            <p className="text-lg text-muted-foreground">{qualification}</p>
+                                                            <p className="text-base font-medium">{years}</p>
+                                                        </div>
+                                                    </div>
+                                                );
+                                            })}
+                                        </div>
+
+                                    </div>
+                                    {/* education */}
+                                    <div>
+
+                                        <div className="flex gap-x-4 mb-4 item-center text-[22px]">
+                                            <GraduationCapIcon size={28} />
+                                            <h3 className="capitalize font-medium">
+                                                {getData(qualificationData, 'education')?.title}
+                                            </h3>
+                                        </div>
+
+
+                                        <div className="flex flex-col gap-y-10">
+                                            {getData(qualificationData, 'education')?.data.map((item, idx) => {
+                                                const { university, qualification, years } = item;
+                                                return (
+                                                    <div key={idx} className="flex gap-x-6 group relative">
+                                                        {/* Timeline line + dot */}
+                                                        <div className="flex flex-col items-center relative">
+                                                            {/* Dot */}
+                                                            <div className="w-3 h-3 rounded-full bg-primary z-10 group-hover:translate-y-[84px] transition-all duration-500 "></div>
+                                                            {/* Line (extends downward) */}
+                                                            {idx !== getData(qualificationData, 'experience')!.data.length - 1 && (
+                                                                <div className="w-[2px] h-full bg-border mt-2"></div>
+                                                            )}
+                                                        </div>
+
+                                                        {/* Content */}
+                                                        <div>
+                                                            <h4 className="font-semibold text-xl leading-tight">{university}</h4>
+                                                            <p className="text-lg text-muted-foreground">{qualification}</p>
+                                                            <p className="text-base font-medium">{years}</p>
+                                                        </div>
+                                                    </div>
+                                                );
+                                            })}
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </TabsContent>
+                            <TabsContent value="skills">
+                                {/* Skills */}
+                                <h4 className="text-xl font-semibold mb-2 xl:text-left">Tools</h4>
+                                <div className="border-b border-border mb-4"></div>
+                                {/* Skills */}
+                                {/* <div className="grid grid-cols-4 md:grid-cols-7 gap-6">
+                                    <Image className="shadow-xl" src="/assets/web/images.png" width={50} height={50} alt=''></Image>
+                                    <Image src="/assets/web/css.png" width={50} height={50} alt=''></Image>
+                                    <Image src="/assets/web/docker.webp" width={50} height={50} alt=''></Image>
+                                    <Image src="/assets/web/express.png" width={50} height={50} alt=''></Image>
+                                    <Image src="/assets/web/figma.png" width={40} height={40} alt=''></Image>
+                                    <Image src="/assets/web/Firebase.png" width={40} height={40} alt=''></Image>
+                                    <Image src="/assets/web/graphql.png" width={50} height={50} alt=''></Image>
+                                    <Image src="/assets/web/html.png" width={50} height={50} alt=''></Image>
+                                    <Image src="/assets/web/js.png" width={50} height={50} alt=''></Image>
+                                    <Image src="/assets/web/logo-sm.png" width={50} height={50} alt=''></Image>
+                                    <Image src="/assets/web/mongodb.png" width={30} height={30} alt=''></Image>
+                                    <Image src="/assets/web/mui.png" width={50} height={50} alt=''></Image>
+                                    <Image src="/assets/web/next.png" width={50} height={50} alt=''></Image>
+                                    <Image src="/assets/web/node-js.png" width={50} height={50} alt=''></Image>
+                                    <Image src="/assets/web/postger.png" width={50} height={50} alt=''></Image>
+                                    <Image src="/assets/web/prisma.webp" width={40} height={40} alt=''></Image>
+                                    <Image src="/assets/web/redux.png" width={50} height={50} alt=''></Image>
+                                    <Image src="/assets/web/react.png" width={50} height={50} alt=''></Image>
+                                    <Image src="/assets/web/tailwind.png" width={50} height={50} alt=''></Image>
+                                    <div className="p-4 rounded-2xl  backdrop-blur-md border  hover:scale-105 transition-transform duration-300 shadow-lg">
+                                        <Image
+                                            src="/assets/web/tailwind.png"
+                                            width={50}
+                                            height={50}
+                                            alt="Tailwind CSS Logo"
+                                            className="object-contain drop-shadow-md"
+                                        />
+                                    </div>
+
+
+
+                                </div> */}
+                                <div className="grid grid-cols-4 md:grid-cols-7 gap-6 place-items-center">
+                                    {[
+                                        "/assets/web/images.png",
+                                        "/assets/web/css.png",
+                                        "/assets/web/docker.webp",
+                                        "/assets/web/express.png",
+                                        // "/assets/web/figma.png",
+                                        // "/assets/web/Firebase.png",
+                                        "/assets/web/graphql.png",
+                                        "/assets/web/html.png",
+                                        "/assets/web/js.png",
+                                        "/assets/web/logo-sm.png",
+                                        // "/assets/web/mongodb.png",
+                                        "/assets/web/mui.png",
+                                        "/assets/web/next.png",
+                                        "/assets/web/node-js.png",
+                                        "/assets/web/postger.png",
+                                        "/assets/web/prisma.webp",
+                                        "/assets/web/redux.png",
+                                        "/assets/web/react.png",
+                                        "/assets/web/tailwind.png",
+                                    ].map((src, idx) => (
+                                        <div
+                                            key={idx}
+                                            className="p-4 rounded-2xl  border 
+                                             hover:scale-110 hover:shadow-[#F3705C]/30 transition-all duration-300 shadow-md 
+                                             flex items-center justify-center group"
+                                        >
+                                            <Image
+                                                src={src}
+                                                width={50}
+                                                height={50}
+                                                alt="Tech Logo"
+                                                className="object-contain drop-shadow-md group-hover:scale-110 transition-transform duration-300"
+                                            />
+                                        </div>
+                                    ))}
+                                </div>
+
+                            </TabsContent>
                         </div>
                     </Tabs>
                 </div>
